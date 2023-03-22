6d38dc52249e0b7273467f6a5c27be8c2c542703
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-03-21 23:24:48 -0300

    initial commit

---
+++ UserRegistrationApi/UserRegistrationApi/Controllers/SigninController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserRegistrationApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SigninController : ControllerBase
+    {
+        [HttpPost(Name = "v1/signin")]
+        public async Task<IActionResult> Signin()
+        {
+            return new OkResult();
+        }
+    }
+}
---
+++ UserRegistrationApi/UserRegistrationApi/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserRegistrationApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController
+    {
+    }
+}
